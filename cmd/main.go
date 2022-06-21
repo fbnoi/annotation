f10cb1be6be83a57cf186b80e97f2ebb3c07d59f
fbnoi <fbnoi@foxmail.com>
2022-06-21 22:53:40 +0800

    Update

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io"
+	"net/http"
 	"os"
 
 	"fbnoi.com/annotation/direct"
@@ -11,6 +13,32 @@ func main() {
 		Name:       "Index",
 		Path:       "/",
 		HandleFunc: "main.Index",
+		Methods:    []string{"GET", "POST"},
 	}
-	router.Render(os.Stdout)
+	err := direct.Render(os.Stdout, router)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//@Route(/post/:id, name=post_show, methods=[GET])
+func Show(req *http.Request, w http.ResponseWriter) {
+	io.WriteString(w, "hello world")
+}
+
+//@Route(/post/:id, name=post_edit, methods=[POST])
+func Edit(req *http.Request, w http.ResponseWriter) {
+	io.WriteString(w, "hello world")
+}
+
+//@Filter({paths:[/post/*], order:1})
+func MD1(req *http.Request, w http.ResponseWriter) {
+}
+
+//@Filter({paths: [/], order: 1})
+func MD2(req *http.Request, w http.ResponseWriter) {
+}
+
+//@Filter({paths: [/], order: 1})
+func MD3(req *http.Request, w http.ResponseWriter) {
 }
--- direct/route.go
+++ direct/route.go
@@ -3,40 +3,60 @@ package direct
 import (
 	"fmt"
 	"io"
+	"net/http"
 	"text/template"
 
 	"fbnoi.com/httprouter"
 )
 
-const ROUTE_TPL = `
-{{ range method .Methods }}
-router.{{ method }}("{{ .Path }}", "{{ .Name }}", "{{ .HandleFunc }}")
+var (
+	tplStr = `
+{{- range .Methods -}}
+router.{{ . }}("{{ $.Path }}", "{{ $.Name }}", "{{ $.HandleFunc }}")
 {{ else }}
-router.All("{{ .Path }}", "{{ .Name }}", "{{ .HandleFunc }}")
-{{ end }}
-`
+router.All("{{ $.Path }}", "{{ $.Name }}", "{{ $.HandleFunc }}")
+{{- end -}}`
+	tpl *template.Template
+)
+
+func Render(wr io.Writer, r *Route) error {
+	if tpl == nil {
+		tpl, _ = template.New(r.Name).Parse(tplStr)
+	}
+	for _, method := range r.Methods {
+		if !httprouter.AllowMethod(method) {
+			panic(fmt.Sprintf("Method %s not allowed", method))
+		}
+	}
+
+	return tpl.Execute(wr, r)
+}
 
 type Route struct {
 	Path       string
 	Name       string
 	Methods    []string
 	HandleFunc string
+	Filters    []*Filter
 }
 
 func (r *Route) GetName() string {
 	return "Route"
 }
 
-func (r *Route) Render(wr io.Writer) error {
-	for _, method := range r.Methods {
-		if !httprouter.AllowMethod(method) {
-			panic(fmt.Sprintf("Method %s not allowed", method))
-		}
-	}
-	tpl, err := template.New(r.Name).Parse(ROUTE_TPL)
-	if err != nil {
-		return err
-	}
+type Filter struct {
+	Path  string
+	Name  string
+	Func  string
+	Order string
+}
 
-	return tpl.Execute(wr, r)
+func (f *Filter) GetName() string {
+	return f.Name
+}
+
+func Chains() httprouter.HandleFunc {
+	return func(r *http.Request, w http.ResponseWriter, ps httprouter.Params) {
+
+	}
 }
